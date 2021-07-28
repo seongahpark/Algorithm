276c42423d33cc3915bdb7b45ded1c0aa363ffcf
sksmsekekek21 <sksmsekekek21@naver.com>
2021-07-28 15:30:37 +0900

    백준 1330 두 수 비교하기

---
+++ boj1330.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int a, b;
+	cin >> a >> b;
+	if (a > b) {
+		cout << ">";
+	}
+	else if (a < b) {
+		cout << "<";
+	}
+	else {
+		cout << "==";
+	}
+	return 0;
+}
